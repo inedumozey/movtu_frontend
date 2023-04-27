2b4cadd0e91177526826a8a776db2da1c2dd3032
inedumozey <inedumozey@gmail.com>
2023-04-26 18:28:20 -0700

    updated

--- pages/collections/index.js
+++ pages/collections/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 
-export default function index() {
+export default function Index() {
     const router = useRouter()
 
     useEffect(() => {
